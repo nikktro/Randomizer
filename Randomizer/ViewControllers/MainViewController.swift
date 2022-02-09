402f9d1a98869f21e1408c276b00eb87a81a1393
Nikolay Trofimov <nikktro@gmail.com>
2022-02-10 00:35:23 +0300

    app interface

---
+++ Randomizer/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Randomizer
+//
+//  Created by Nikolay Trofimov on 09.02.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+
+
+
+}
+
