d961693222905328b144969f6ec0aa5173876882
Nikolay Trofimov <nikktro@gmail.com>
2022-02-11 03:29:05 +0300

    settingsVC logic

--- Randomizer/ViewControllers/MainViewController.swift
+++ Randomizer/ViewControllers/MainViewController.swift
@@ -20,11 +20,23 @@ class MainViewController: UIViewController {
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.minimumValue = minimumValueLabel.text
+        settingsVC.maximumValue = maximumValueLabel.text
+    }
+    
     @IBAction func getRandomNumberButtonPressed() {
         guard let minimumNumber = Int(minimumValueLabel.text ?? "0") else { return }
         guard let maximumNumber = Int(maximumValueLabel.text ?? "100") else { return }
         
         randomValueLabel.text = String(Int.random(in: minimumNumber...maximumNumber))
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        minimumValueLabel.text = settingsVC.minimumValueTF.text
+        maximumValueLabel.text = settingsVC.maximumValueTF.text
     }
 
 }
--- Randomizer/ViewControllers/SettingsViewController.swift
+++ Randomizer/ViewControllers/SettingsViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = maximumValue
+    }
+    
+    @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
     }
     
 }
