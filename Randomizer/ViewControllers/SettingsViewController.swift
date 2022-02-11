6726a2992949c4249c1934470802376fed077964
Nikolay Trofimov <nikktro@gmail.com>
2022-02-11 03:00:52 +0300

    added settingsVC

---
+++ Randomizer/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Nikolay Trofimov on 11.02.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
